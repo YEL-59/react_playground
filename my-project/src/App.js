c3d29c7412cb872a58b84bc0140d1275407d10e3
YEL-59 <rokomari144@gmail.com>
2023-02-01 15:59:53 +0600

    (feat):adding cart ,show selected data,price tax total price

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,14 +1,15 @@
 
 import './App.css';
+import Cosmetics from './Component/Cosmetics/Cosmetics';
 import Product from './Component/Product/Product';
 import TryVideo from './Component/TryVideo';
 function App() {
   return (
-    <>
+    <> 
     <Product></Product>
 
     {/* <TryVideo/> */}
-  
+ 
     </>
   );
 }
--- my-project/src/Component/Product/Product.jsx
+++ my-project/src/Component/Product/Product.jsx
@@ -6,6 +6,23 @@ import Card from "../Card/Card";
 const Product = () => {
   const [products, setproducts] = useState([]);
   const [cart, setCart] = useState([]);
+
+  let total=0
+  for(const product of cart){
+    total=total+product.price
+
+  }
+
+  let shipingCost=0
+  for( const product of cart){
+    shipingCost=shipingCost+product.shipping
+  }
+
+
+  let grandTotal=total+shipingCost
+
+
+
   useEffect(() => {
     fetch("products.json")
       .then((res) => res.json())
@@ -46,6 +63,11 @@ const Product = () => {
 
               <div className="selected_item border rounded p-1">
                 <p className="text-blue-600 font-medium">Selected Item :<span className=" text-[#089bab] font-bold p-1 shadow-2xl ">{cart.length}</span></p>
+                <p className="text-blue-600 font-medium">Total Price :<span className=" text-[#089bab] font-bold p-1 shadow-2xl ">${total}</span></p>
+                <p className="text-blue-600 font-medium">Total Shipping :<span className=" text-[#089bab] font-bold p-1 shadow-2xl ">{cart.length}</span></p>
+                <p className="text-blue-600 font-medium">tax:<span className=" text-[#089bab] font-bold p-1 shadow-2xl ">{cart.length}</span></p>
+                <p className="text-blue-600 font-medium">Grand Total :<span className=" text-[#089bab] font-bold p-1 shadow-2xl ">{grandTotal}</span></p>
+                <p className="text-blue-600 font-medium">Shipping Cost :<span className=" text-[#089bab] font-bold p-1 shadow-2xl ">{shipingCost}</span></p>
               </div>
 
               <p className="text-gray-700 text-base mb-4">
