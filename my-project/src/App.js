9cfd5359ebf61c16f4d2afd5d8c3c20414e58096
YEL-59 <rokomari144@gmail.com>
2023-02-03 16:23:27 +0600

    (feat):basic router

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -5,11 +5,26 @@ import Price from "./Component/Priceing/Price";
 import Chart from "./Component/Chart/chart";
 import Product from "./Component/Product/Product";
 import Phonebar from "./Component/Phonebar/Phonebar";
-
+import {RouterProvider, createBrowserRouter} from 'react-router-dom';
 //import TryVideo from './Component/TryVideo';
 function App() {
+  const router= createBrowserRouter([
+    {
+      path:'/',element:<div>
+        this is ok page
+      </div>
+    },
+    {
+      path: '/home' ,element:<div>Home PAge</div>
+    },
+    {
+      path: 'about' ,element:<div>Home2 PAge</div>
+    }
+    
+  ])
   return (
     <>
+    <RouterProvider router={router}></RouterProvider>
       <Navbar></Navbar>
       <Product></Product>
       <Price></Price>
