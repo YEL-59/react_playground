62a8275cfc24bf2d0d35d6cc70a89ef0d6bc74e2
YEL-59 <rokomari144@gmail.com>
2023-07-18 03:05:16 +0600

    star rating project for ecommecre moving rating

---
+++ LAVEL_1/Star_Rating/src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+//import App from "./App";
+import StarRating from "./StarRating";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <StarRating message={["Terrible", "Ok", "Good", "Very Good", "Excellent"]}
+     
+    />
+    {/* <StarRating size={24} color="red" className="test" defaultRating={2} /> */}
+
+   
+  </React.StrictMode>
+);
